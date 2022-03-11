2cd453dad54d5080edde7971d3a8ea608debe4bc
RomanKuzhekov <profet@inbox.ru>
2022-03-11 23:22:47 +0300

    Создаем контроллеры приложения

---
+++ controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 11.03.2022
+ * Time: 23:21
+ */
+
+namespace controllers;
+
+class Controller
+{
+
+}
---
+++ controllers/ParserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 11.03.2022
+ * Time: 23:22
+ */
+
+namespace controllers;
+
+
+class ParserController
+{
+
+}
