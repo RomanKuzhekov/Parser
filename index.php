52842b5a47ea2f7be552603edf05b7c82bcd9ec2
RomanKuzhekov <profet@inbox.ru>
2022-03-11 22:58:18 +0300

    Делаем автолоадер

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Роман
- * Date: 11.03.2022
- * Time: 22:55
- */
---
+++ services/Autoloader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 11.03.2022
+ * Time: 22:57
+ */
+
+namespace services;
+
+
+class Autoloader
+{
+
+}
